ca856eb2c2f9069cebc9c7c277d1a4b2e3d03648
lemontech119 <lemontech119@gmail.com>
2019-09-27 17:41:40 +0900

    집 가기 전 커밋

--- app.js
+++ app.js
@@ -3,8 +3,32 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const path=require("path");
 const session = require('express-session');
+const flash = require('connect-flash');
+require('dotenv').config();
+
 const app=express(); 
 const port = 3000;
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie:{
+        httpOnly: true,
+        secure: false
+    }
+}));
+
+app.use(flash());
 
 
 app.get('/', (req, res) => {
