f14626429eed1df2477dd1dc5b804ce96f4481b6
dongwon <lemontech119@gmail.com>
2019-10-24 14:25:50 +0900

    📝 cors 추가

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app = express();
 
 app.use(bodyParser.urlencoded({
   extended: false
 }))
+app.use(bodyParser.json());
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
-app.use(bodyParser.json());
+app.use(cors());
 app.use(routes);
 
 module.exports = app;
