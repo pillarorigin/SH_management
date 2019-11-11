265501ad70aef4376867cecaa99203e4753887a2
tony <lall21@naver.com>
2019-11-11 16:02:09 +0900

    📝: 코드변경

--- client/cat/src/Header.js
+++ client/cat/src/Header.js
@@ -1,50 +1,49 @@
 import React, { Component } from 'react';
-import { Link , withRouter} from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import './Header.css';
 import Axios from 'axios';
-
+import { Button , ButtonGroup, Breadcrumb} from 'reactstrap';
 class Header extends Component {
-
-    
     logout = () => {
         localStorage.removeItem('userId');
         this.props.loginState();
         this.props.history.push('/');
     }
-    list = () =>{
+    list = () => {
         const name = JSON.parse(localStorage.getItem('userId'));
         const url = `http://localhost:4000/users/${name}`;
         const data = {
-            userId : name
+            userId: name
         }
         const config = {
             headers: {
                 'Content-Type': 'application/json'
             }
         }
-        Axios.get(url,data,config)
-        .then((response) => {
-            console.log('res',response)
-            localStorage.setItem('data',JSON.stringify(response.data.result[0]))
-            console.log(localStorage)
-            this.props.history.push('/mypage')
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
+        Axios.get(url, data, config)
+            .then((response) => {
+                console.log('res', response)
+                localStorage.setItem('data', JSON.stringify(response.data.result[0]))
+                console.log(localStorage)
+                this.props.history.push('/mypage')
+            })
+            .catch((error) => {
+                console.log(error)
+            })
     }
     menuset = () => {
         if (localStorage.getItem('userId')) {
-
             return (
                 <div>
                     <div className="head-div">
-                        <Link className="head-link" to='/'>Home</Link>
-                        <Link className="head-link" to='/info'>Info</Link>
-                        <Link className="head-link" to='/donation'>Donation</Link>
+                        <ButtonGroup>
+                    <Button outline color="secondary"><Link style={{color:"black"}} to='/'>Home</Link></Button>
+                    <Button outline color="secondary"><Link style={{color:"black"}} to='/info'>Info</Link></Button>
+                    <Button outline color="secondary"><Link style={{color:"black"}} to='/donation'>Donation</Link></Button>
                         {/* <Link className="head-link" to='/mypage' onClick={this.list}>MyPage</Link> */}
-                        <button className="head-link head-button" onClick={this.list}>MyPage</button>
-                        <button className="head-link head-button" onClick={this.logout}>Logout</button>
+                        <Button onClick={this.list}>MyPage</Button>
+                        <Button onClick={this.logout}>Logout</Button>
+                        </ButtonGroup>
                     </div>
                 </div>
             )
@@ -52,11 +51,13 @@ class Header extends Component {
             return (
                 <div>
                     <div className="head-div">
-                        <Link className="head-link" to='/'>Home</Link>
-                        <Link className="head-link" to='/info'>Info</Link>
-                        <Link className="head-link" to='/donation'>Donation</Link>
-                        <Link className="head-link" to='/login'>Login</Link>
-                        <Link className="head-link" to='/sign'>Sign</Link>
+                    <ButtonGroup>
+                        <Button outline color="secondary"> <Link style={{color:'black'}} to='/'>Home</Link> </Button>
+                        <Button outline color="secondary"> <Link style={{color:'black'}} to='/info'>Info</Link></Button>
+                        <Button outline color="secondary"> <Link style={{color:'black'}} to='/donation'>Donation</Link></Button>
+                        <Button outline color="secondary"> <Link style={{color:'black'}} to='/login'>Login</Link></Button>
+                        <Button outline color="secondary"> <Link style={{color:'black'}} to='/sign'>Sign</Link></Button>
+                        </ButtonGroup>
                     </div>
                 </div>
             )
@@ -67,8 +68,8 @@ class Header extends Component {
         var menu = this.menuset()
         return (
             <div>
-                <h1>길냥이</h1>
                 {menu}
+                <h1 align="center" style ={{fontFamily: 'Sunflower', fontSize:'80px'}}> 길냥이 이야기</h1>
             </div>
         )
     }
